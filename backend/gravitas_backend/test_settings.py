a10f199f3d70c6a95a22bb62bf9347cbe1efd678
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-08-13 23:46:58 +0330

    Add isolated Django test settings

---
+++ backend/gravitas_backend/test_settings.py
@@ -0,0 +1,11 @@
+from .settings import *  # noqa: F401,F403
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'test.sqlite3',
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+PASSWORD_HASHERS = ['django.contrib.auth.hashers.MD5PasswordHasher']
