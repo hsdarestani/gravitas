3c88f3669e63e7cb42afddd8b2b18c8fa704ea92
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-08-29 16:13:43 +0330

    Mirror research project membership into granular ACL

--- backend/core/platform_signals.py
+++ backend/core/platform_signals.py
@@ -1,8 +1,9 @@
-from django.db.models.signals import post_save
+from django.contrib.contenttypes.models import ContentType
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
-from .models import WorkspaceMembership
-from .platform_models import WorkspaceProfile
+from .models import ProjectMembership, WorkspaceMembership
+from .platform_models import AccessGrant, WorkspaceProfile
 
 
 @receiver(post_save, sender=WorkspaceProfile)
@@ -17,3 +18,34 @@ def isolate_research_workspace_from_legacy_acl(sender, instance, **kwargs):
     """
     if instance.purpose == WorkspaceProfile.Purpose.RESEARCH:
         WorkspaceMembership.objects.filter(workspace=instance.workspace).delete()
+
+
+@receiver(post_save, sender=ProjectMembership)
+def mirror_project_membership_to_granular_acl(sender, instance, **kwargs):
+    """Make project membership usable by every V2 endpoint without broad ACLs."""
+    role = {
+        ProjectMembership.Role.OWNER: AccessGrant.Role.MANAGE,
+        ProjectMembership.Role.EDITOR: AccessGrant.Role.EDIT,
+        ProjectMembership.Role.VIEWER: AccessGrant.Role.VIEW,
+    }.get(instance.role, AccessGrant.Role.VIEW)
+    content_type = ContentType.objects.get_for_model(instance.project, for_concrete_model=False)
+    AccessGrant.objects.update_or_create(
+        content_type=content_type,
+        object_id=instance.project_id,
+        user=instance.user,
+        defaults={
+            'role': role,
+            'granted_by': instance.project.owner,
+            'expires_at': None,
+        },
+    )
+
+
+@receiver(post_delete, sender=ProjectMembership)
+def remove_project_membership_grant(sender, instance, **kwargs):
+    content_type = ContentType.objects.get_for_model(instance.project, for_concrete_model=False)
+    AccessGrant.objects.filter(
+        content_type=content_type,
+        object_id=instance.project_id,
+        user=instance.user,
+    ).delete()
