dba6d439157907fc9eafac43c506d08ff556a661
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-08-31 11:37:21 +0330

    Register Space filesystem models

--- backend/core/apps.py
+++ backend/core/apps.py
@@ -11,5 +11,6 @@ class CoreConfig(AppConfig):
         from . import operating_models  # noqa: F401
         from . import platform_models  # noqa: F401
         from . import roadmap_models  # noqa: F401
+        from . import space_models  # noqa: F401
         from . import platform_signals  # noqa: F401
         from . import operating_admin  # noqa: F401
