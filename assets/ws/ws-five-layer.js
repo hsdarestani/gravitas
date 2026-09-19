3febc54a2634212ccfe5d9af19ad6806952d2f07
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-09-19 20:52:02 +0330

    Refresh final learner LMS bundle

--- assets/ws/ws-five-layer.js
+++ assets/ws/ws-five-layer.js
@@ -8,7 +8,7 @@ import {
   renderMemberLibrary,
   renderMemberOverview,
   renderMyLearning,
-} from './ws-member-lms.js?v=20260919-advanced6';
+} from './ws-member-lms.js?v=20260919-advanced7';
 import { renderMemberProgress } from './ws-member-progress.js?v=20260918-progress3';
 import { renderMemberSupport } from './ws-support.js?v=20260918-support1';
 import {
