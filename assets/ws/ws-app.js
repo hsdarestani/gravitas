00e96bd8249b8a74ffd8ec5a3447c94f19b4c8f9
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-09-19 22:36:53 +0330

    Bust task-board module cache after clean reset

--- assets/ws/ws-app.js
+++ assets/ws/ws-app.js
@@ -21,7 +21,7 @@
 
 import * as api from './ws-api.js?v=20260913-3';
 import * as P from './ws-platform.js?v=20260919-planning1';
-import * as views from './ws-views.js?v=20260919-planning1';
+import * as views from './ws-views.js?v=20260919-tasks-clean1';
 import * as assets from './ws-core-assets.js?v=20260919-assets3';
 import * as kms from './ws-kms-views.js';
 import * as library from './ws-library.js';
