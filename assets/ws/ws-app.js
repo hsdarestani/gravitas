5e5ef739a702099f1894159323d359e276d650a5
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-09-20 19:31:56 +0330

    Complete visual4 workspace cache chain

--- assets/ws/ws-app.js
+++ assets/ws/ws-app.js
@@ -21,7 +21,7 @@
 
 import * as api from './ws-api.js?v=20260913-3';
 import * as P from './ws-platform.js?v=20260919-planning1';
-import * as views from './ws-views.js?v=20260920-dashboard3';
+import * as views from './ws-views.js?v=20260920-visual4';
 import * as assets from './ws-core-assets.js?v=20260920-pulsar1';
 import * as kms from './ws-kms-views.js';
 import * as library from './ws-library.js';
