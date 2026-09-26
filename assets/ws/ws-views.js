6ad21bf44a0fd7c18910de4e83058a8dee815c73
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-09-26 21:12:37 +0330

    Align task view Calendar module versions

--- assets/ws/ws-views.js
+++ assets/ws/ws-views.js
@@ -8,8 +8,8 @@
    jump when data lands, and why a failure has somewhere obvious to render.
    ========================================================================== */
 
-import * as P from './ws-platform.js?v=20260926-calendar1';
-import { WORKSPACES, availableWorkspaces } from './ws-nav.js?v=20260919-planning1';
+import * as P from './ws-platform.js?v=20260926-calendar2';
+import { WORKSPACES, availableWorkspaces } from './ws-nav.js?v=20260926-meetings1';
 
 const icon = (name) => window.GravitasIcons.icon(name, 'g-wi');
 
